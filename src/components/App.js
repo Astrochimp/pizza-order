45b7fb581aab4cf7958b66511ef0118cc81de12a
Marcello Prattico <chimp@astrochimp.com>
2019-05-12 18:00:29 -0400

    setup initial query for pizzas

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 
-function App() {
-  return (
-    <div>
-      Pizza Order
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    pizzaMenu: []
+  }
+
+  componentDidMount() {
+    const client = new ApolloClient({
+      uri: 'https://core-graphql.dev.waldo.photos/pizza'
+    });
+
+    this.setState({
+      client,
+      pizzaMenu: []
+    })
+
+    client.query({
+      query: gql`{
+        pizzaSizes {
+          name,
+          maxToppings,
+          basePrice,
+          toppings {
+            pizzaSize {
+              maxToppings
+            },
+            topping {
+              name,
+              price
+            }
+            defaultSelected
+          }
+        }
+      }
+      `
+    }).then(data => {
+        console.log('data', data.data.pizzaSizes)
+        this.setState({
+          pizzaMenu: data.data.pizzaSizes
+        })
+      }
+    )
+    .catch(error => console.error(error));
+  }
+
+  render() {
+
+    const pizzaList = this.state.pizzaMenu.map((pizza, ind) => {
+      return (
+        <div key={ind}>
+          <h1>{pizza.name}</h1>
+          <div>Max Toppings: {pizza.maxToppinga}</div>
+          <div>Base Price: {pizza.basePrice}</div>
+          <div>Toppings</div>
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        Pizza Order
+        {pizzaList}
+      </div>
+    );
+    }
 }
 
 export default App;
